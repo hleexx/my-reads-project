6ae8e318363ed71658bc721ff2be44503d755b64
Hellen Lee <hleexx@gmail.com>
2018-10-01 17:27:27 -0500

    Add access to router inside the app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import ( BrowserRouter ) from 'react-router-dom'
+
+
+/* To use routing inside of the app */
+ReactDOM.render(
+	<BrowserRouter>
+	<App />
+	<BrowserRouter>,
+	document.getElementById('root')
+)
