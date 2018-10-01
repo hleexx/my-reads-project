0da3c3ee586f03c3919cd2d095f3e29e0a0b9214
Hellen Lee <hleexx@gmail.com>
2018-10-01 17:30:35 -0500

    Book filtering and re-rendering on the shelf

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../../BooksAPI'
+
+import Shelf from '../Shelf';
+
+/* Adds empty array for 
+class MainPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			books: []
+		}
+	}
+
+	/* Coming from BooksAPI and causes re-render */
+	componentDiDMount() {
+		BooksAPI.getAll()
+		.then(resp => {
+			this.setState({ books: resp });
+		});
+	}
+	/* Filter books and re-add it back */
+	updateBook = (book, shelf) => {
+		BooksAPI.update(book, shelf)
+		.then(resp => {
+			book.shelf = shelf;
+			this.setState(state => ({
+				books: state.books.filter(b => b.id !== book.id).concat([book])
+			}));
+		});
+	}
+
+	render() {
+		return (
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
+				<div className="list-books-content">
+					<div>
+					/* Passing the name of the shelf and all the books to render */
+						<Shelf updateBook={this.updateBook} name="Currently Reading" books={this.state.book.filter(b => b.shelf === "currentlyReading")} />
+						<Shelf updateBook={this.updateBook} name="want To Read" books={this.state.book.filter(b => b.shelf === "wantToRead")} />
+						<Shelf updateBook={this.updateBook} name="Read" books={this.state.book.filter(b => b.shelf === "read")} />
+					</div>
+				</div>
+				<div className="open-search">
+					<Link to "/search">Add a book</link>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+export default MainPage;
